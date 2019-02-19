bfffa51fab61f5cee13d1443da91e36dd328df4d
Pavol Risa <risapav@gmail.com>
2019-02-19 19:25:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# (c)Peter Riša
+
+## Školský projekt "Kolektor"
+ - Návrh solárneho kolektora na ohrev teplej vody.
+
+## Dokumenty
+ - Ekologické kúrenie 1500 slov
+ - Ekologické kúrenie
+ - Osnova
+ - PT_prepis
+ 
+## Hardware
+ - 1x Arduino Mega 2560 R3 alebo UNO R3 ATmega328P
+ - 1x Prototypová doska pre Arduino MEGA 2560 - Shield Board
+ - 3x DS18B20 teplomer vodeodolný s káblom
+ - 1x čerpadlo na vodu 
+ - 1x relé doska 
+ - 1x display
+ 
---
+++ src/main.cpp
@@ -0,0 +1,149 @@
+/*
+* Arduino
+*
+* (c) Peter Riša 2019
+* 
+* riadenie ohrevu vody cez slnečný kolektor
+*/
+#include <Arduino.h>
+#include <Wire.h>
+
+// knižnice pre komunikáciu s teplomermi Dallas DS1820
+#include <OneWire.h>
+#include <DallasTemperature.h>
+// knižnica pre zobrazovanie na LCD
+#include <LiquidCrystal.h>
+
+// konštanty pre riadenie čerpadla
+#define CERPADLO_CHOD 1
+#define CERPADLO_STOP 0
+
+// konštanty pre nadefinovanie displeja LCD display 1602A
+#define LCD_WIDTH 16
+#define LCD_HEIGHT 2
+
+// konštanty pre zapnutie ohrevu
+#define TEPLOTA_MAX 45.0
+#define TEPLOTA_VIAC 5.0
+#define TEPLOTA_MENEJ 3.0
+
+// koľko môže byť DS18S20 teplomerov pripojených
+#define MAX_DS1820_SENSORS 5
+// pole teplôt      
+// teplota[0] = T1 - teplota kolektora
+// teplota[1] = T2 - teplota v bojleri
+// teplota[2] = T3 - teplota okolia
+float teplota[MAX_DS1820_SENSORS]; 
+
+// definovanie vstupov a výstupov arduina
+int relayPin = 3; // relé je na pine 3
+int num_temp = 0; //pocet teplomerov pripojenych
+// vytvoření instance oneWireDS z knihovny OneWire
+OneWire  oneWireDS(13);  // teplomery sú na pine 13
+// vytvoření instance senzoryDS z knihovny DallasTemperature
+DallasTemperature senzoryDS(&oneWireDS);
+
+byte addr[MAX_DS1820_SENSORS][8];
+char buf[20];
+// definovanie rozhrania displeja (rs, enable, d4, d5, d6, d7)
+LiquidCrystal lcd(1, 2, 4, 5, 6, 7);
+
+// na začiatok čerpadlo stojí
+int cerpadlo = CERPADLO_STOP;
+    
+// priprav všetko pred spustením regulácie
+void setup() { 
+
+    // najprv predpokladame, že čerpadlo nejde
+    cerpadlo = CERPADLO_STOP;
+
+    // iniciacia rele modulu
+    pinMode(relayPin, OUTPUT);  //Set pin for output
+    digitalWrite(relayPin, HIGH);  // HIGH is off, LOW is on
+
+    // začni komunikovať s displejom
+    lcd.begin(LCD_WIDTH, LCD_HEIGHT); 
+
+    //napis uvodny text
+    lcd.setCursor(0,0); // riadok hore
+    lcd.print("(c) Peter Risa");
+    lcd.setCursor(0,1); // riadok dole
+    lcd.print("Slnecny kolektor");
+    delay(3000); // 3 seconds delay 
+
+    // začni komunikovať s teplomermi
+    senzoryDS.begin();
+
+    //zisti počet teplomerov
+    lcd.clear(); // Clears the LCD screen
+    lcd.setCursor(0,0); // riadok hore
+    lcd.print("DS1820 Test");
+
+    byte i;
+    for(i=0;i < MAX_DS1820_SENSORS;i++) {  
+        if (!oneWireDS.search(addr[i])) { 
+            lcd.setCursor(0,1); // riadok dole
+            lcd.print("No more addresses.");
+            oneWireDS.reset_search();
+            delay(250);
+            break;        
+        }
+        num_temp ++; // počet teplomerov zvýš o 1
+        sprintf(buf, "Pocet teplomerov: %d",num_temp);
+        lcd.setCursor(0,1); // riadok dole
+        lcd.print(buf);
+        delay(2000);
+    }
+    lcd.setCursor(0,0); // riadok hore
+    lcd.print("Aktualny stav");
+}
+   
+// riadenie ohrevu
+void loop() { 
+    // načíta všetky teplomery
+    byte sensor;
+    senzoryDS.requestTemperatures();
+
+    // uloží teploty do poľa teplota
+    for (sensor=0;sensor < num_temp;sensor++) {
+        teplota[sensor]=senzoryDS.getTempCByIndex(sensor);
+    }      
+
+    //  čerpadlo má bežať ak teploty sú aspoň 2
+    if(num_temp<2)
+        return;
+
+    // ak teplota T1 (teplota kolektora) je o TEPLOTA_VIAC väčšia
+    // ako T2 (teplota v bojleri), potom spusti čerpadlo
+    if(teplota[0] > (teplota[1] + TEPLOTA_VIAC))
+        cerpadlo=CERPADLO_CHOD;
+
+    // ak teplota T1 (teplota kolektora) bez TEPLOTA_MENEJ je menšia 
+    // ako T2 (teplota v bojleri), potom vypni čerpadlo
+    if((teplota[0] - TEPLOTA_MENEJ) < teplota[1] )
+        cerpadlo=CERPADLO_STOP;
+
+    // ak teplota T2 (teplota v bojleri) je viac ako TEPLOTA_MAX, 
+    // zastav čerpadlo, aby sme neprehriali bojler
+    if(teplota[1] > TEPLOTA_MAX)
+        cerpadlo=CERPADLO_STOP;
+
+    //teraz skutočne vypni alebo zapni čerpadlo    
+    if(cerpadlo == CERPADLO_CHOD) {
+        // zapni čerpadlo
+        digitalWrite(relayPin, LOW);
+    } else {
+        // vypni čerpadlo
+        digitalWrite(relayPin, HIGH);
+    }
+
+    //zobraz teplotu na displej uloz ich do pola teplot
+    for (sensor=0;sensor < num_temp;sensor++) {
+        lcd.setCursor(0,1);
+        sprintf(buf, "C: %d, T%d: ",cerpadlo, sensor+1);
+        lcd.print(buf);
+        lcd.print(teplota[sensor]);
+        delay(1000);
+    }
+
+}
